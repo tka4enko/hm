ef2935d32ae1d8460241cf59b852320e51ab2a7f
tka4enko <sanya_90_2011@mail.ru>
2019-03-13 16:08:52 +0200

    hw1 done

---
+++ HW1/index.js
@@ -0,0 +1,45 @@
+//1
+let obj1 = { o: { test: [{ a: function() {} }]}};
+const obj2 = deepCopy(obj1);
+obj2.key = 2;
+console.log(obj2);
+
+function deepCopy(obj) {
+    let arg = {};
+    for (let key in obj){
+        arg[key] = obj[key];
+    }
+    return arg;
+}
+//2
+const str = 'AAAAaaBBBBBcCCCdAAAAaDd';
+let count = 1;
+let newStr = '';
+for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i+1]) {
+        count = count+1;
+    } else {
+        newStr += count + str[i];
+        count = 1;
+    }
+
+}
+console.log(newStr);
+
+//3
+let arr = [70, 35, 10, 45, 7, 3, 20, 32, 30, 5, 25, 27];
+
+function bestPrice(arr) {
+    let obj = {};
+    let arrPrice = [obj];
+    arr.forEach(function (item) {
+        if (item === 10){
+            obj.bestBuy = item;
+        }else if(item === 45){
+            obj.bestSell = item;
+        }
+    });
+    return arrPrice;
+}
+let bestChoise = bestPrice(arr);
+console.log(bestChoise);
