4919055263c95aa1a921b9c67e2cc2cfda51e5ca
Alexander Tkachenko <sanya_90_2011@mail.ru>
2019-03-20 16:49:03 +0200

    updated hm2

--- HW2/index.js
+++ HW2/index.js
@@ -1,98 +1,139 @@
 let objectCalc = {
     results:'',
+    value:'',
+    check:{
+        add:false,
+        substitute:false,
+        multiply:false,
+        divide:false,
+    },
     zero:function() {
-        objectCalc.results += "0";
+        objectCalc.value = 0;
+        this.mathResults();
         this.updateValue();
         return objectCalc;
     },
     one:function() {
-        objectCalc.results += "1";
+        objectCalc.value = 1;
+        this.mathResults();
         this.updateValue();
         return objectCalc;
     },
     two:function() {
-        objectCalc.results += "2";
+        objectCalc.value = 2;
+        this.mathResults();
         this.updateValue();
         return objectCalc;
     },
-
     three:function(){
-        objectCalc.results += "3";
+        objectCalc.value = 3;
+        this.mathResults();
         this.updateValue();
         return objectCalc;
     },
 
     four:function() {
-        objectCalc.results += "4";
+        objectCalc.value = 4;
+        this.mathResults();
         this.updateValue();
         return objectCalc;
     },
 
     five:function(){
-        objectCalc.results += "5";
+        objectCalc.value = 5;
+        this.mathResults();
         this.updateValue();
         return objectCalc;
     },
 
     six:function() {
-        objectCalc.results += "6";
+        objectCalc.value = 6;
+        this.mathResults();
         this.updateValue();
         return objectCalc;
     },
 
     seven:function(){
-        objectCalc.results += "7";
+        objectCalc.value = 7;
+        this.mathResults();
         this.updateValue();
         return objectCalc;
     },
 
     eight:function() {
-        objectCalc.results += "8";
+        objectCalc.value = 8;
+        this.mathResults();
         this.updateValue();
         return objectCalc;
     },
 
     nine:function() {
-        objectCalc.results += "9";
+        objectCalc.value = 9;
+        this.mathResults();
         this.updateValue();
         return objectCalc;
     },
 
     add:function() {
-        objectCalc.results += "+";
+        this.check.add = true;
         this.updateValue();
         return objectCalc;
     },
     substitute:function() {
-        objectCalc.results += "-";
+        this.check.substitute = true;
         this.updateValue();
         return objectCalc;
     },
     multiply:function() {
-        objectCalc.results += "*";
+        this.check.multiply = true;
         this.updateValue();
         return objectCalc;
     },
 
     divide:function() {
-        objectCalc.results += "/";
+        this.check.divide =  true;
         this.updateValue();
         return objectCalc;
     },
     equal:function() {
-        objectCalc.results = eval(this.results);
         this.updateValue();
-        objectCalc.results = '';
+        console.log(this.results);
+        document.getElementsByClassName('calc-item')[0].value = this.results;
+        this.results = '';
         return objectCalc;
     },
+    updateMathParams:function(){
+        for (key in this.check ){
+            this.check[key] = false;
+        }
+    },
+    mathResults:function(){
+        if (this.results){
+            if (this.check.add){
+                this.results = this.results + this.value;
+            }
+            if (this.check.divide){
+                this.results = this.results /this.value;
+            }
+            if (this.check.multiply){
+                this.results = this.results * this.value;
+            }
+            if (this.check.substitute){
+                this.results = this.results - this.value;
+            }
+        }else{
+            this.results = this.value;
+        }
+        this.updateMathParams();
+    },
     reset:function() {
-
-        objectCalc.results = '';
+        this.results = '';
+        this.value = '';
         this.updateValue();
         return objectCalc;
     },
     updateValue:function () {
-        document.getElementsByClassName('calc-item')[0].value = this.results;
+        document.getElementsByClassName('calc-item')[0].value = this.value;
     }
 };
 
@@ -100,42 +141,42 @@ let objectCalc = {
 // console.log(six().substitute().five().equal());
 // // почему если передать objectCalc как параметр в функции то не находит методы в объекте
 function zero(){
-    objectCalc.results += "0";
+    objectCalc.results = 0;
     return objectCalc;
 }
 function one(){
-    objectCalc.results += "1";
+    objectCalc.results = 1;
     return objectCalc;
 }
 function two(){
-    objectCalc.results += "2";
+    objectCalc.results = 2;
     return objectCalc;
 }
 function three(){
-    objectCalc.results += "3";
+    objectCalc.results = 3;
     return objectCalc;
 }
 function four(){
-    objectCalc.results += "4";
+    objectCalc.results = 4;
     return objectCalc;
 }
 function five(){
-    objectCalc.results += "5";
+    objectCalc.results = 5;
     return objectCalc;
 }
 function six(){
-    objectCalc.results += "6";
+    objectCalc.results = 6;
     return objectCalc;
 }
 function seven(){
-    objectCalc.results += "7";
+    objectCalc.results = 7;
     return objectCalc;
 }
 function eight(){
-    objectCalc.results += "8";
+    objectCalc.results = 8;
     return objectCalc;
 }
 function nine(){
-    objectCalc.results += "9";
+    objectCalc.results = 9;
     return objectCalc;
 }
