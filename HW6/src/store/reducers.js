86588b3a6ef0448efdc0007a9dae4e2cb89a5af6
Alexander Tkachenko <sanya_90_2011@mail.ru>
2019-05-06 19:00:14 +0300

    updated with undo not full

--- HW6/src/store/reducers.js
+++ HW6/src/store/reducers.js
@@ -1,11 +1,13 @@
 import {BUTTON_CLICK} from "../components/Number/actions";
 import {
+  BUTTON_UNDO_CLICK,
   BUTTON_COMMAND_CLICK,
   EQUAL_CLICK,
-  CLEAR_CLICK
+  CLEAR_CLICK,
 } from "../components/Command/actions";
 
 const initialState = {
+  results: "",
   first: "",
   second: "",
   operation: "",
@@ -13,23 +15,55 @@ const initialState = {
 
 const formatResult = state => ({
   ...state,
-  result: state.first + state.operation + state.second,
+  result: state.results,
 });
+
+const generateArgs = (expression) => {
+  let copy = expression;
+  expression = expression.replace(/[0-9]+/g, "#");
+  let numbers = copy.split(/[^0-9\.]+/);
+  let operators = expression.split("#").filter(function (n) {
+    return n
+  });
+  let result = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    result.push(numbers[i]);
+    if (i < operators.length){
+      result.push(operators[i]);
+    }
+  }
+  return result;
+};
+
 const countResult = state => {
   let result = 0;
-  console.log(state);
-  if (state.operation === '+' && state.second.length) {
-    result = Number(state.first) + Number(state.second);
-  }
-  if (state.operation === '-' && state.second.length) {
-    result = state.first - state.second;
-  }
-  if (state.operation === '*' && state.second.length) {
-    result = state.first * state.second;
-  }
-  if (state.operation === '/' && state.second.length) {
-    result = state.first / state.second;
-  }
+  const args = generateArgs(state.results);
+  args.map((item, i) => {
+    if (item === '+') {
+      if (!result) {
+        result = Number(args[i - 1]) + Number(args[i + 1]);
+      } else {
+        result = result + Number(args[i + 1]);
+      }
+    }
+    if (item === '*') {
+      console.log(args[i]);
+      if (!result) {
+        result = args[i - 1] * args[i + 1];
+      } else {
+        result = result * args[i + 1];
+      }
+    }
+    if (item === '/') {
+      console.log(args[i]);
+      if (!result) {
+        result = args[i - 1] / args[i + 1];
+      } else {
+        result = result / args[i + 1];
+      }
+    }
+  });
   return result;
 };
 
@@ -37,27 +71,13 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case BUTTON_CLICK: {
       const result = Object.assign({}, state);
-      if (!state.operation) {
-        result.first += action.data;
-        return formatResult(result);
-      }
-      result.second += action.data;
+      result.results += action.data;
       return formatResult(result);
     }
     case BUTTON_COMMAND_CLICK: {
-      if(!!state.operation){
-        return Object.assign(
-          {},
-          {
-            count: countResult(state),
-            result: false
-          },
-          {...initialState}
-        );
-      }else{
-        const result = Object.assign({}, state, {operation: action.data});
-        return formatResult(result);
-      }
+      const result = Object.assign({}, state,{operation: action.data});
+      result.results += action.data;
+      return formatResult(result);
     }
     case EQUAL_CLICK: {
       return Object.assign(
@@ -68,6 +88,9 @@ export default (state = initialState, action) => {
         },
         {...initialState}
       );
+    }
+    case BUTTON_UNDO_CLICK: {
+      console.log(state);
     }
     case CLEAR_CLICK: {
       return initialState;
